05893f234671e263dbadab00fb1d00fe5b0b25f4
tavoralex <alex.tavor@gmail.com>
2019-07-13 20:03:49 +0300

    Updates

---
+++ precache-manifest.072ff7272c36b6aa8802b97140491081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0edbb0228bbd210220d2096914970bc",
+    "url": "/skedaddle/index.html"
+  },
+  {
+    "revision": "ee5d9c4bca7da7d6f71f",
+    "url": "/skedaddle/static/css/main.496e2034.chunk.css"
+  },
+  {
+    "revision": "9099a609309e37035e5e",
+    "url": "/skedaddle/static/js/2.d33a53b1.chunk.js"
+  },
+  {
+    "revision": "ee5d9c4bca7da7d6f71f",
+    "url": "/skedaddle/static/js/main.fba4a2ac.chunk.js"
+  },
+  {
+    "revision": "22fdadd4f964f6f0e144",
+    "url": "/skedaddle/static/js/runtime~main.85ff9bdf.js"
+  },
+  {
+    "revision": "74496d9086d97aaeeafb3085e9957668",
+    "url": "/skedaddle/static/media/PressStart2P.74496d90.ttf"
+  }
+]);
